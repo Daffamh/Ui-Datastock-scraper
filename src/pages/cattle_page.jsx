2ea759fa8edeab50c9453931be7c8a0b02c9652f
Daffamh <daffa.muhadzdzab39@smk.belajar.id>
2025-10-01 14:38:26 +0700

    menambahkan abattoir di cattle

--- src/pages/cattle_page.jsx
+++ src/pages/cattle_page.jsx
@@ -1,27 +1,26 @@
 import DataTable from "../components/DataTable.jsx";
-import {fetchCattle} from "../repository/index.js";
-import {useEffect, useState} from "react";
+import { fetchCattle } from "../repository/index.js";
+import { useEffect, useState } from "react";
 import React from "react";
 
 function CattlePage() {
     const [data, setData] = useState([]);
-
-    const displayKeys = ["rfid","import_date", "abattoir_arrive_date", "slaughter_date"]
+    const displayKeys = ["rfid", "import_date", "abattoir_name", "abattoir_arrive_date", "slaughter_date"];
 
     useEffect(() => {
         fetchCattle().then(response => {
-                setData(response)
-            }
-        );
-    }, [])
+            const transformedData = response.map(cattle => ({
+                ...cattle,
+                abattoir_name: cattle.abattoir?.name || "–"
+            }));
+            setData(transformedData);
+        });
+    }, []);
 
-    return (React.createElement(DataTable,{
+    return React.createElement(DataTable, {
         dataSource: data,
         displayKeys: displayKeys,
-    })
-
-    );
+    });
 }
-
 
 export default CattlePage;
